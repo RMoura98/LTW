8daf4ce6d713af03df8e489931cd54486a83f58e
Moura <up201604912@fe.up.pt>
2018-12-09 00:26:34 +0000

    :(

--- php/action_create_post.php
+++ php/action_create_post.php
@@ -3,45 +3,6 @@ include_once('connection.php');
 include_once('../includes/session.php');
 include_once('../sql/db_user.php');
 
-/* if(empty($_POST['title']) || empty($_POST['text']) || empty($_FILES['img']) || empty($_POST['tags']) ){
-    header('Location: ../php/createPost');
-    exit();
-} 
-
-
-
-
-
-$picUrl = upload_img($_FILES['img']);
-
-$fulltags = str_replace(' ', '', $_POST['tags']);
-$fulltags = str_replace(';', '#', $fulltags);
-$fulltags = str_replace(',', '#', $fulltags);
-$fulltags =  explode('#',$fulltags);
-$tags = "";
-foreach ($fulltags as $tag) {
-    if($tag != ''){
-        $tags .= $tag;
-        if( next( $fulltags ) && next( $fulltags ) != '') {
-            $tags .=  ",";
-        }
-    }
-}
-
-
-try {
-    $lastId = insertPost($_POST['title']  , $tags, $_SESSION['username'], $_POST['text'], $picUrl);
-    header('Location: ' . '../php/item?id=' . $lastId);
-    exit();
-
-} catch (PDOException $e) {
-    die($e->getMessage());
-    $_SESSION['messages'][] = array('type' => 'error', 'content' => 'Failed to signup!');
-    header('Location: createPost');   
-    exit();
-}  */
-
-
 if(empty($_POST['text'])){
     $_POST['text'] = '';
 } 
